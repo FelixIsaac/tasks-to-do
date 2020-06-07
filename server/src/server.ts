a4b4a1f669079fda719dfe262113c4b3b66f2fc0
Felix Isaac <limfelix16@gmail.com>
2020-06-08 05:30:53 +0800

    Parse JSON requests

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,11 @@
 import Koa from 'koa';
+import bodyParser from "koa-bodyparser";
 import apiRoutes from './routes/api-routes';
+import("./db")
 
 const app = new Koa();
 
+app.use(bodyParser());
 app.use(apiRoutes.routes());
 app.use(apiRoutes.allowedMethods());
 
