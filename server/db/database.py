c4ef9b8adf0697845a3ee1021f4cb3f8857f20aa
Felix Isaac <limfelix16@gmail.com>
2020-06-07 23:56:48 +0800

    Initial commit

--- server/db/database.py
+++
@@ -1,8 +0,0 @@
-import json
-from mongoengine import connect
-
-with open("config.json") as data:
-    config = json.load(data)
-    config = config[config["env"]]
-
-connection = connect(host=config["database"])
--- server/db/models/list.py
+++
@@ -1,9 +0,0 @@
-from mongoengine import *
-from .task import Task
-
-
-class List(Document):
-    name = StringField(required=True, unique=True)
-    description = StringField()
-    icon = StringField()
-    tasks = ListField(ReferenceField(Task))
