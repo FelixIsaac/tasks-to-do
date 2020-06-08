4c7f47ea41a70390c02ccd1a127bad2cf93aea40
Felix Isaac <limfelix16@gmail.com>
2020-06-08 12:32:56 +0800

    Fixed "Able to create multiple accounts using the same email address"

--- server/src/utils/encryption.ts
+++ server/src/utils/encryption.ts
@@ -1,5 +1,12 @@
-import CryptoJS from "crypto-js";
-const key = "ThisIsAVerySecretSecretiveKey...$$$"
+import aesjs from "aes-js";
+const key = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 , 11, 12, 13, 14, 15, 16];
 
-export const encrypt = (text: string) => CryptoJS.AES.encrypt(text, key).toString();
-export const decrypt = (text: string) => CryptoJS.AES.decrypt(text, key).toString(CryptoJS.enc.Utf8);
+export const encrypt = (text: string) => {
+  const aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
+  return aesjs.utils.hex.fromBytes(aesCtr.encrypt(aesjs.utils.utf8.toBytes(text)));
+};
+
+export const decrypt = (text: string) => {
+  const aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
+  return aesjs.utils.utf8.fromBytes(aesCtr.decrypt(aesjs.utils.hex.toBytes(text)));
+};
