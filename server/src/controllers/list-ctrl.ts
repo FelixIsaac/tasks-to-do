32724afcf17f0b0c47948f823f4e04b265b3434e
Felix Isaac <limfelix16@gmail.com>
2020-06-14 20:45:40 +0800

    Add "Update task title"

--- server/src/controllers/list-ctrl.ts
+++ server/src/controllers/list-ctrl.ts
@@ -274,3 +274,55 @@ export const createTask = async (cookie: string, ip: string, title: ITaskDocumen
     message: "Created task"
   };
 };
+
+export const verifyTaskOwner = async (cookie: string, ip: string, taskID: ITaskDocument["_id"]) => {
+  if (!taskID) throw {
+    error: true,
+    status: 400,
+    message: "Missing list ID"
+  };
+
+  const user = await getUserByCookie(cookie, ip);
+
+  if (!user) throw {
+    error: true,
+    status: 400,
+    message: "Invalid email or password"
+  }
+
+  const list = await Lists.findOne({ "tasks._id": taskID });
+
+  if (!list) throw {
+    error: true,
+    status: 400,
+    message: "Task or list does not exist"
+  };
+
+  return { user, list, owner: list.user.toString() === user._id.toString() };
+};
+
+export const updateTaskTitle = async (cookie: string, ip: string, newTitle: ITaskDocument["title"], taskID: ITaskDocument["_id"]) => {
+  if (!newTitle || !taskID) throw {
+    error: true,
+    status: 400,
+    message: "Missing new task title"
+  };
+
+  const { list, owner } = await verifyTaskOwner(cookie, ip, taskID);
+
+  if (!owner) throw {
+    error: true,
+    status: 401,
+    message: "Unauthorized to perform this action"
+  };
+
+  list.tasks.id(taskID).title = sanitize(newTitle);
+  await list.save()
+
+  return {
+    error: false,
+    status: 200,
+    message: "Updated task title"
+  };
+};
+
--- server/src/routes/list.ts
+++ server/src/routes/list.ts
@@ -133,5 +133,35 @@ router.post("/:id", async (ctx) =>{
   }
 });
 
+router.patch("/:listID/tasks/:taskID/:action", async (ctx) => {
+  const { listID, taskID , action } = ctx.params;
+  const session = ctx.cookies.get("session");
+  const ip = ctx.ip;
+
+  if (!session) {
+    ctx.status = 401;
+    return ctx.body = {};
+  }
+
+  try {
+    switch(action.toLowerCase()) {
+      case "title": {
+        const response = await listCtrl.updateTaskTitle(session, ip, ctx.request.body.newTitle, taskID);
+
+        ctx.status = 200;
+        ctx.body = response;
+        break;
+      }
+      default:
+        ctx.status = 404;
+        return ctx.body = "Not Found";
+    }
+  } catch (err) {
+    if (!err.status) console.error(err);
+
+    ctx.status = err.status || 500;
+    ctx.body = err;
+  }
+});
 
 export default router;
